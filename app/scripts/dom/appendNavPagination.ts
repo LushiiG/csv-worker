e3a44f754d3a1ff16cd74a5c8dea98a0940df979
Lush Gojani <lushgojani@gmail.com>
2025-09-28 12:21:28 +0200

    feat: pagination ui implementation

---
+++ app/scripts/dom/appendNavPagination.ts
@@ -0,0 +1,49 @@
+const LIMIT_OF_ITEMS_PER_TABLE = 500000;
+
+export const appendNavPagination = (
+  lengthOfFile: number,
+  currentPage: number
+) => {
+  const mainContainer = document.querySelector(".main-container")!;
+  console.log(mainContainer, "mainContainer");
+  if (lengthOfFile < LIMIT_OF_ITEMS_PER_TABLE) return;
+  const ul = document.createElement("ul");
+  ul.className = "pagination";
+  const totalPages = Math.ceil(lengthOfFile / LIMIT_OF_ITEMS_PER_TABLE);
+  console.log(totalPages, "totalPages");
+  const prevPage = document.createElement("li");
+  prevPage.innerHTML = `<li><button class="page-btn" ${
+    currentPage > 1 ? "" : "disabled"
+  }>Previous</button></li>`;
+
+  prevPage.addEventListener("click", () => {
+    //logic here
+  });
+
+  ul.appendChild(prevPage);
+  for (let i = 0; i < totalPages; i++) {
+    const page = document.createElement("li");
+    const pageNumber = i + 1;
+    page.innerHTML = `<li><button class="page-btn ${
+      pageNumber === currentPage ? "active" : ""
+    }" >${pageNumber}</button></li>`;
+
+    page.addEventListener("click", () => {
+      console.log("other logic here");
+    });
+
+    ul.appendChild(page);
+  }
+
+  const nextPage = document.createElement("li");
+  nextPage.innerHTML = `<li><button class="page-btn" ${
+    currentPage > totalPages ? "disabled" : ""
+  }>Next</button></li>`;
+
+  nextPage.addEventListener("click", () => {
+    //logic here
+  });
+
+  ul.appendChild(nextPage);
+  mainContainer.append(ul);
+};
